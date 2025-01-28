3dad536fb21af738ceff66f10fd15962b6195d03
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-28 13:10:01 +0300

    upd

--- conftest.py
+++ conftest.py
@@ -7,14 +7,11 @@ from selenium import webdriver
 def browser_setup():
     # Настройки
     options = webdriver.ChromeOptions()
-    # options.page_load_strategy = 'eager'
     browser.config.driver_options = options
     browser.config.browser_name = 'chrome'
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-
-    browser.open('/automation-practice-form')
 
     yield
 
