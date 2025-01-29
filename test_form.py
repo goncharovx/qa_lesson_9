028d8e282d90ed5ba835cf50e124551ae0b22218
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-29 22:18:56 +0300

    optimize

--- test_form.py
+++ test_form.py
@@ -6,48 +6,37 @@ from selene.support.shared.jquery_style import s
 
 
 def test_form_submission_with_s_and_banner_removal():
-    # Удаление рекламных баннеров
     browser.driver.execute_script("$('#fixedban').remove()")
     browser.driver.execute_script("$('footer').remove()")
 
-    # Путь к файлу
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, 'resources', 'pic.png')
 
-    # Заполнение формы
     s('#firstName').type('Sonic')
     s('#lastName').type('Syndicate')
     s('#userEmail').type('test@mail.ru')
     s('[for="gender-radio-1"]').click()  # Male
     s('#userNumber').type('9939993388')
 
-    # Выбор даты рождения
     s('#dateOfBirthInput').click()
     s('.react-datepicker__month-select').click().s('[value="2"]').click()  # Март (03)
     s('.react-datepicker__year-select').click().s('[value="1960"]').click()  # 1960
     s('.react-datepicker__day--003:not(.react-datepicker__day--outside-month)').click()  # 03
 
-    # Заполнение поля предметов
     s('#subjectsInput').type('Maths').press_enter()
 
-    # Выбор хобби
     s('[for="hobbies-checkbox-1"]').click()  # Sports
     s('[for="hobbies-checkbox-3"]').click()  # Music
 
-    # Загрузка файла
     s('#uploadPicture').send_keys(file_path)
 
-    # Ввод адреса
     s('#currentAddress').type('Moscow 5')
 
-    # Выбор штата и города
     s('#state').click().s('#react-select-3-option-0').click()  # NCR
     s('#city').click().s('#react-select-4-option-0').click()  # Delhi
 
-    # Отправка формы
     s('#submit').click()
 
-    # Проверка таблицы результатов
     s('.table-responsive').should(have.text('Sonic Syndicate'))
     s('.table-responsive').should(have.text('test@mail.ru'))
     s('.table-responsive').should(have.text('Male'))
